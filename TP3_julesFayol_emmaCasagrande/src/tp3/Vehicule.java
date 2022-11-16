45c027728916ca1ca6a803800869739a32c0946f
emmacasagrande <emmacasagrande@localhost>
2022-11-16 15:52:24 +0100

    J'ai commencé à ajouter la methode ajouter pour la classe agence

--- TP3_julesFayol_emmaCasagrande/src/tp3/Vehicule.java
+++ TP3_julesFayol_emmaCasagrande/src/tp3/Vehicule.java
@@ -12,26 +12,34 @@ public class Vehicule {
 //    rose violet bleu magenta vert jaune orange rouge marron noir gris cyan blanc brun kaki louvet taupe bai beige smoke green purple red schwarz lila
 //            gelb rot weiss mein kleines Kind 
 //                    du bist dumm
-    
-    private String gamme, identifiant, genre;
-    private int nbplace;
-    
-        
-    public Vehicule (String gamme, String identifiant, String genre, int nbplace){
-     this.gamme=gamme;
-     this.identifiant=identifiant;
-     this.genre=genre;
-     this.nbplace=nbplace;
-}
-    // attibuts
-    
-    
-    // constucteur
-    
-    @Override
-    public String toString(){
-        
-        return ("la gamme de la voiture est : " + gamme + " le genre du vehicule est : " + genre + " ,le nombre de place de vehicule est de  " + nbplace + " places, l'identifiant du vehicule est "+ identifiant +" ");
-        
+
+    private String gamme, identifiant;
+    private int nbplace, genre;
+
+    public Vehicule(String gamme, String identifiant, int genre, int nbplace) {
+        this.gamme = gamme;
+        this.identifiant = identifiant;
+        this.genre = genre;
+        this.nbplace = nbplace;
     }
+    // attibuts
+
+    // constucteur
+    @Override
+    public String toString() {
+
+        return ("la gamme de la voiture est : " + gamme + " le genre du vehicule est : " + genre + " ,le nombre de place de vehicule est de  " + nbplace + " places, l'identifiant du vehicule est " + identifiant + " ");
+
+    }
+
+    
+   public boolean placerApres(int genrevoiture) { 
+        int genre = this.genre;
+        int compare = genre - genrevoiture; 
+        if(compare > 0){
+            return false;
+        }
+        return true;
+    }
+
 }
